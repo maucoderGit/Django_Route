8bb2a7a394f80c6f1210993470757fc3325692e6
Mau <dreg5000.bbb@gmail.com>
2022-06-11 19:45:21 -0400

    Models

--- bestseriesawards/polls/models.py
+++ bestseriesawards/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date_published")
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choise_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
