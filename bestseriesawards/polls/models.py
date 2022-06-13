77bf430f425a8c076a828a0110b12c21c544e20b
Mau <dreg5000.bbb@gmail.com>
2022-06-13 16:55:35 -0400

    Django Admin

--- bestseriesawards/polls/models.py
+++ bestseriesawards/polls/models.py
@@ -1,13 +1,23 @@
+from datetime import datetime
 from django.db import models
+from django.utils import timezone
 
 # models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date_published")
+    pub_date = models.DateTimeField("date_published", auto_now_add=True)
 
-class Choices(models.Model):
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choise_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
+
+    def __str__(self):
+        return self.choise_text
