1e13e4a281cad0e893137830c82af91bab19e5a2
Mau <dreg5000.bbb@gmail.com>
2022-06-13 18:10:53 -0400

    Templates and Views

--- bestseriesawards/polls/views.py
+++ bestseriesawards/polls/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from.models import Question
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    lattest_question_list = Question.objects.all() 
+    return render(request, 'polls/index.html', {
+        'lattest_question_list': lattest_question_list,
+    })
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You\'re watching the question number {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'You\'re watching the results from the question number {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You\'re voting the question number {question_id}')
