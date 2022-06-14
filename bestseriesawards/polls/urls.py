e58c389b2a88ada934d62a83646b023e08cac360
Mau <dreg5000.bbb@gmail.com>
2022-06-13 20:42:31 -0400

    Template Tags, url, for, if

--- bestseriesawards/polls/urls.py
+++ bestseriesawards/polls/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # Example: /polls/
     path('', views.index, name='index'),
     # Example: /polls/5/
-    path('<int:question_id>/', views.detail, name='question'),
+    path('<int:question_id>/detail/noimportaloquehagasaldrabien', views.detail, name='detail'),
     # Example: /polls/5/results
-    path('<int:question_id>/results/', views.results, name='question'),
+    path('<int:question_id>/results/', views.results, name='results'),
     # Example: /polls/5/results
-    path('<int:question_id>/vote/', views.vote, name='question')
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- bestseriesawards/polls/views.py
+++ bestseriesawards/polls/views.py
@@ -1,24 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from.models import Question
 
 # Create your views here.
 
-def index(request):
+def index(request: HttpResponse):
+    """
+    Index Function view.
+
+    Get all polls
+
+    Parameters:
+    - Request: Request
+
+    Returns:
+    - Render response with:
+        - HttpResponse.
+        - Url
+        - Questions: Dict[Object]
+    """
     lattest_question_list = Question.objects.all() 
     return render(request, 'polls/index.html', {
         'lattest_question_list': lattest_question_list,
     })
 
 
-def detail(request, question_id):
-    return HttpResponse(f'You\'re watching the question number {question_id}')
+def detail(request: HttpResponse, question_id: int):
+    """
+    Polls details
+
+    Get a question by id
+
+    Args:
+    - request: Request
+    - question_id: int
+
+    Returns:
+    - - Render response with:
+        - Request.
+        - Url
+        - Questions: Dict[id]
+    """
+    question: Question | None = get_object_or_404(Question, pk=question_id)
+    
+    return render(request,'polls/detail.html', context={
+        'question': question
+    })
 
 
-def results(request, question_id):
+def results(request: HttpResponse, question_id):
     return HttpResponse(f'You\'re watching the results from the question number {question_id}')
 
 
-def vote(request, question_id):
+def vote(request: HttpResponse, question_id):
     return HttpResponse(f'You\'re voting the question number {question_id}')
