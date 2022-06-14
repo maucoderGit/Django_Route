3cf4361cf340018c4852276c947fa2e0567f9bf7
Mau <dreg5000.bbb@gmail.com>
2022-06-14 10:42:14 -0400

    views

--- bestseriesawards/polls/models.py
+++ bestseriesawards/polls/models.py
@@ -16,8 +16,8 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choise_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choise_text
+        return self.choice_text
--- bestseriesawards/polls/views.py
+++ bestseriesawards/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from.models import Question
+from.models import Choice, Question
 
 # Create your views here.
 
@@ -49,9 +50,55 @@ def detail(request: HttpResponse, question_id: int):
     })
 
 
-def results(request: HttpResponse, question_id):
-    return HttpResponse(f'You\'re watching the results from the question number {question_id}')
+def results(request: HttpResponse, question_id: int):
+    """
+    Results
+
+    Get questions results
+
+    Args:
+    - request: HttpResponse
+    - question_id: int
+
+    Returns:
+    - httpResponse:
+        - redirect: polls/results.html
+        - context: dict[str: Question]
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', context={
+        'question': question
+    })
 
 
-def vote(request: HttpResponse, question_id):
-    return HttpResponse(f'You\'re voting the question number {question_id}')
+def vote(request: HttpResponse, question_id: int):
+    """
+    Vote
+
+    Vote for a choice
+
+    Args:
+    - request: HttpResponse
+    - question_id: int
+
+    Returns:
+    - if there's a id, redirect to results view.
+    - else render again the detail view with an error message.
+    """
+    # First we get a question using the id getted in the form
+    question = get_object_or_404(Question, pk=question_id)
+
+    # If there's a id we redirect to results view, else, we render again the
+    # detail view with the error message.
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", context={
+            'question': question,
+            'error_message': 'You must select an option'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
